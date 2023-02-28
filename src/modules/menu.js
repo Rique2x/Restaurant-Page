4bd836a843eafc9196beb5c629244d6d5022bde8
Rique2 <enrique2heinrich@gmail.com>
2023-02-28 23:34:22 +0200

    add meals pics

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -10,6 +10,29 @@ const renderMenuPage = (() => {
         <h3>Menu</h3>
         <p>our special meals</p>
     </div>
+    <div class="list">
+    <div class="list-1">
+    <img src="/dist/meals/fishandchips_413x549-7bk06tn9p4g69tt8fe4wvkjcc.jpeg" alt="">
+        <div class="prices">
+        <p>Fish & amp; Chips</p>
+        <p>N$80</p>
+    </div>
+    </div>
+    <div class="list-2">
+    <img src="/dist/meals/platter-for-2-7c2g6imsfvj2lw03nem43qqy4.png" alt="">
+        <div class="prices">
+        <p>Platter for 2</p>
+        <p>N$359</p>
+    </div>
+    </div>
+    <div class="list-3">
+    <img src="/dist/meals/prawn_mussel_413x549-7bk06twyq1u8moprnb69jlt64.jpeg" alt="">
+        <div class="prices">
+        <p>Prawn & Mussel combo</p>
+        <p>N$134</p>
+    </div>
+    </div>
+</div>
   </div>`;
   
     contentContainer.appendChild(menu);
